b0ecc7c5643fcb20a6a8cf8df4c0e8792466b4eb
ManuelVC <manuel.161293@gmail.com>
2016-05-25 17:12:38 -0500

    Problema de fusion resuelto en routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,15 @@ Route::get('movies/{category}',function(){
 Route::get('movies/{id}',function(){
   return view('pages.movies.show');
 })->where('id','^[0-9]+');
+
+Route::get('account',function(){
+  return view('pages.users.account');
+});
+
+Route::get('reset',function(){
+  return view('pages.users.reset');
+});
+
+Route::get('reset/change',function(){
+  return view('pages.users.change');
+});
